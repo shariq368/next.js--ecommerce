156b6d3137bea304fcc1a6904ea94c66079faf0b
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-06-15 12:17:57 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
     images: {
         domains: ["cdn.sanity.io"],
       },
+      
     };
 
 
