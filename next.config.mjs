128625c53925ac07af7bca52e58ff521a011ac44
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-06-19 16:54:22 +0500

    change

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,14 @@ import { config } from 'process';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode:false,
-  webpack:true,
-  webpack:(config)=> {
-    config.resolve.fallback={fs:false}
-    return config
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-    images: {
-        domains: ["cdn.sanity.io"],
-      },
-      
-    };
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+
+};
 
 
 export default nextConfig;
